832869fd26af03107c6cc421182928d5d506131e
birkenkrahe <birkenkrahe@lyon.edu>
2025-08-28 17:22:28 -0500

    Update from thelio

---
+++ src/ovissa1.cpp
@@ -0,0 +1,10 @@
+// Gets a single line of input and prints the number of 'u' characters
+#include <iostream>
+
+int main()
+{
+  std::string input;
+  getline(std::cin, input);
+  std::cout << input.size() << '\n';
+  return 0;
+}
